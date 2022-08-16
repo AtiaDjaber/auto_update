5ab2f669320bcc561b80b09f50859f7758ae80e7
User <atiadjaber39@gmail.com>
2022-08-16 10:17:59 +0100

    add nsis

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,22 @@
 module.exports = {
-  transpileDependencies: [
-    'vuetify'
-  ]
-}
+  pluginOptions: {
+    electronBuilder: {
+      nodeIntegration: true,
+      builderOptions: {
+        productName: "Express Store",
+        appId: "express_store.com",
+        win: {
+          target: ["nsis"],
+          icon: "public/icon.ico",
+          // "requestedExecutionLevel": "requireAdministrator"
+        },
+        nsis: {
+          oneClick: false,
+          allowToChangeInstallationDirectory: true,
+        },
+        files: ["**/*"],
+      },
+    },
+  },
+  transpileDependencies: ["vuetify"],
+};
