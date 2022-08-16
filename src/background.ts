e4941293c912f65bf205a02092ebf9837fede739
User <atiadjaber39@gmail.com>
2022-08-16 15:55:50 +0100

    fix issues

--- src/background.ts
+++ src/background.ts
@@ -1,18 +1,23 @@
 'use strict'
 
-import { app, protocol, BrowserWindow , autoUpdater, dialog } from 'electron'
+import { app, protocol, BrowserWindow, autoUpdater, dialog, ipcMain } from 'electron'
+const { autoUpdater } = require('electron-updater')
+
 import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
 import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
+
+
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
 // Scheme must be registered before the app is ready
 protocol.registerSchemesAsPrivileged([
   { scheme: 'app', privileges: { secure: true, standard: true } }
 ])
+let win:any;
 
 async function createWindow() {
   // Create the browser window.
-  const win = new BrowserWindow({
+   win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
@@ -34,6 +39,10 @@ async function createWindow() {
     // Load the index.html when not in development
     win.loadURL('app://./index.html')
   }
+  
+   win.once('ready-to-show', () => {
+    autoUpdater.checkForUpdatesAndNotify();
+  });
 }
 
 // Quit when all windows are closed.
@@ -60,7 +69,7 @@ app.on('ready', async () => {
     try {
       await installExtension(VUEJS_DEVTOOLS)
     } catch (e) {
-      console.error('Vue Devtools failed to install:', e.toString())
+      // console.error('Vue Devtools failed to install:', e.toString())
     }
   }
   createWindow()
@@ -80,26 +89,69 @@ if (isDevelopment) {
     })
   }
 }
-
-const server = 'https://auto-updateas-8mav4riqx-atiadjaber.vercel.app';
-const url = `${server}/update/${process.platform}/${app.getVersion()}`;
-
-autoUpdater.setFeedURL({ url })
-
-const UPDATE_CHECK_INTERVAL = 10 * 60 * 1000
-setInterval(() => {
-  autoUpdater.checkForUpdates()
-}, UPDATE_CHECK_INTERVAL)
-
-autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
-  const dialogOpts = {
+autoUpdater.on('update-available', (_event:any, releaseNotes:any, releaseName:any) => {
+  // win.webContents.send('update_available');
+    const dialogOpts = {
     type: 'info',
     buttons: ['Restart', 'Later'],
     title: 'Application Update',
     message: process.platform === 'win32' ? releaseNotes : releaseName,
     detail: 'A new version has been downloaded. Restart the application to apply the updates.'
   }
-dialog.showMessageBox(dialogOpts).then((returnValue) => {
-    if (returnValue.response === 0) autoUpdater.quitAndInstall()
-  })
-})
+     dialog.showMessageBox(dialogOpts);
+
+  console.log('update-available');
+
+});
+
+autoUpdater.on('update-downloaded', (_event:any, releaseNotes:any, releaseName:any) => {
+  const dialogOpts = {
+      type: "info",
+      buttons: ['Restart', 'Later'],
+      title: 'Application Update',
+      message: process.platform === 'win32' ? releaseNotes : releaseName,
+      detail: 'A new version has been downloaded. Restart the application to apply the updates.'
+   };
+   dialog.showMessageBox(dialogOpts).then((returnValue) => {
+      if (returnValue.response === 0) autoUpdater.quitAndInstall()
+   });});
+let updateNotAvailable = false;
+
+autoUpdater.on("update-not-available", (_event:any) => {
+    const dialogOpts = {
+        type: 'info',
+        buttons: ['Ok'],
+        title: `Update Not available for ${autoUpdater.channel}`,
+        message: "A message!",
+        detail: `Update Not available for ${autoUpdater.channel}`
+    };
+
+    if (!updateNotAvailable) {
+        updateNotAvailable = true;
+        dialog.showMessageBox(dialogOpts);
+    }
+});
+
+
+// const server = 'https://auto-updateas-8mav4riqx-atiadjaber.vercel.app';
+// const url = `${server}/update/${process.platform}/${app.getVersion()}`;
+
+// autoUpdater.setFeedURL({ url })
+
+// const UPDATE_CHECK_INTERVAL = 10 * 60 * 1000
+// setInterval(() => {
+//   autoUpdater.checkForUpdates()
+// }, UPDATE_CHECK_INTERVAL)
+
+// autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+//   const dialogOpts = {
+//     type: 'info',
+//     buttons: ['Restart', 'Later'],
+//     title: 'Application Update',
+//     message: process.platform === 'win32' ? releaseNotes : releaseName,
+//     detail: 'A new version has been downloaded. Restart the application to apply the updates.'
+//   }
+// dialog.showMessageBox(dialogOpts).then((returnValue) => {
+//     if (returnValue.response === 0) autoUpdater.quitAndInstall()
+//   })
+// })
